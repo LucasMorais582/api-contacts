b2b1ab7137687d121c68556fa733068ae5954c4e
Lucas <lucasmorais582@gmail.com>
2020-09-03 01:07:15 -0300

    migration da tabela contato

--- database/migrations/2020_09_02_233701_create_contatos_table.php
+++ database/migrations/2020_09_02_233701_create_contatos_table.php
@@ -15,6 +15,9 @@ class CreateContatosTable extends Migration
     {
         Schema::create('contatos', function (Blueprint $table) {
             $table->id();
+            $table->string('name',100);
+            $table->string('telefone',20);
+            $table->string('email',100)->unique();
             $table->timestamps();
         });
     }
